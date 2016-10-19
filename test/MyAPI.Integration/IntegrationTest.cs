0c53b56fe4a27bb299db2b82aa77271ffe17573b
glennc <glennc@microsoft.com>
2016-10-18 22:54:54 -0700

    initial

---
+++ test/MyAPI.Integration/IntegrationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyAPI.Integration
+{
+    public class IntegrationTest
+    {
+        private HttpClient _httpClient;
+        public IntegrationTest()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        [Fact]
+        public void CanGet200()
+        {
+            var test = _httpClient.GetAsync("http://myapi/api/values").Result;
+            Assert.True(test.IsSuccessStatusCode);
+        }
+    }
+}
